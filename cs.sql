22adfa196e85b9cc83e31681c85b896dc531b172
Ildana <ildana.ruzybayeva@gmail.com>
2020-03-18 06:56:52 +0000

    add food_category table and link to food_menu

--- cs.sql
+++ cs.sql
@@ -47,20 +47,30 @@ CREATE TABLE food_menu
     is_vegan BOOLEAN NOT NULL
 );
 
+DROP TABLE IF EXISTS food_category;
+CREATE TABLE food_category
+(
+    category_id SERIAL PRIMARY KEY,
+    category_name VARCHAR(100) NOT NULL,
+    category_description VARCHAR(100) NOT NULL
+);
+
 
 INSERT INTO purchase
     (purchase_price, purchase_method, purchase_date, purchase_from_member, purchase_type, purchase_items)
 VALUES
     (250, 'card', '2016-06-22 19:10:25-07', false, 'breakfast combo', 'coffee and cheese sandwich'),
-    (99, 'card', now(), true, 'lunch combo', 'soup and tea'),
+    (99, 'card', '2019-01-05', true, 'lunch combo', 'soup and tea'),
     (50, 'swish', '2017-06-22 19:10:25-07', true, 'lunch combo', 'soup and tea');
 
 
 INSERT INTO staff
     (first_name, last_name, personal_number, contact, address_info, salary, sex, bdate, employment_date, contract_type, title)
 VALUES
-    ('Ildana', 'Ruzybayeva', 9511148166, '0760759618', 'Malmo', 120, 'F', '1995-11-14', '2019-07-01', 'Full-time', 'Barista'),
-    ('Alva', 'Rosqvist', 9511148166, '07564315', 'Malmo', 120, 'F', '1993-08-14', '2019-04-01', 'Part-time', 'Barista');
+    ('John', 'Doe', 9052631485, '0760759618', 'Malmo', 30000, 'M', '1990-11-14', '2018-01-01', 'Full-time', 'Boss'),
+    ('Adam', 'Adamson', 9511148166, '0760759618', 'Malmo', 22000, 'M', '1995-11-14', '2018-01-01', 'Full-time', 'Barista'),
+    ('Ildana', 'Ruzybayeva', 874065056, '0760759618', 'Malmo', 22000, 'F', '1987-11-14', '2019-05-01', 'Full-time', 'Barista'),
+    ('Alva', 'Rosqvist', 6698751124, '07564315', 'Malmo', 15000, 'F', '1966-08-14', '2019-07-01', 'Part-time', 'Barista');
 
 
 INSERT INTO drinks_menu
@@ -85,3 +95,19 @@ VALUES
     ('Ham and Cheese grilled sandwich', 67, 'sandwich', false),
     ('Granola youghurt', 29, 'youghurt', false),
     ('Egg sandwich', 45, 'sandwich', false);
+
+
+INSERT INTO food_category
+    (category_name, category_description)
+VALUES
+    ('salad', 'four types of salads based on shrimps, oumph, chicken and halloumi'),
+    ('sandwich', 'grilled sandwiches, rolls and bagels'),
+    ('youghurt', 'granola and nuts options'),
+    ('fika', 'chocolate, gluten-free, milk-free, sugar-free');
+
+
+    --Linking food_menu and food_category 
+    SELECT * 
+    FROM food_menu
+    JOIN food_category
+    ON food_menu.food_category = food_category.category_name;
